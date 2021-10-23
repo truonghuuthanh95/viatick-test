f2de1f943e9de1201d2dc5f5a643de7850b75b57
Truong Huu Thanh <truonghuuthanh95@gmail.com>
2021-10-23 15:55:28 +0700

    remove unnecessary things

--- front-end/src/app/store.js
+++ front-end/src/app/store.js
@@ -1,9 +1,7 @@
 import { configureStore } from "@reduxjs/toolkit";
-import photoReducer from "features/Photo/photoSlice";
 import authSlice from "features/Auth/authSlice";
 export const store = configureStore({
   reducer: {
-    photo: photoReducer,
     auth: authSlice,
   },
 });
--- front-end/src/routes/index.js
+++ front-end/src/routes/index.js
@@ -1,7 +1,6 @@
 import React, { Suspense } from "react";
 import { BrowserRouter, Redirect, Route, Switch } from "react-router-dom";
 
-const Photo = React.lazy(() => import("features/Photo/index"));
 const Auth = React.lazy(() => import("features/Auth/index"));
 const Index = () => {
   return (
